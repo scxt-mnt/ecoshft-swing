91ada7de9a943e774cf2b96bf226f263c208a658
Scott <scottboragay06@gmail.com>
2025-10-02 18:59:41 +0800

    added: login form and loading screen

---
+++ src/javaapplication13/JavaApplication13.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication13;
+
+/**
+ *
+ * @author borag
+ */
+public class JavaApplication13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        loadingFrame frame = new loadingFrame();
+        frame.setVisible(true);
+    }
+    
+}
